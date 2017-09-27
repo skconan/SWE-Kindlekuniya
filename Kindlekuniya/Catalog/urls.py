e9d57beb8a1f420ffdecdf92af2f76d1e3a44480
Pattnatt <jittapat007@gmail.com>
2017-09-27 20:35:05 +0700

    create index page

--- Kindlekuniya/Catalog/urls.py
+++ Kindlekuniya/Catalog/urls.py
@@ -1,1 +1,8 @@
 from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # /catalog/ -> catalog homepage
+    url(r'^$', views.index, name = 'index'),
+
+]
--- Kindlekuniya/Catalog/views.py
+++ Kindlekuniya/Catalog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+def index(request):
+    context = {
+    }
+    return render(request, 'index.html', context)
