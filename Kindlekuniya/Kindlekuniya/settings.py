8138ac67d95b8930a0f36c5c545cad5a7eedefdb
Pattnatt <jittapat007@gmail.com>
2017-09-27 17:57:23 +0700

    Revert "Update database to MySQL"
    This reverts commit 36b196d5855a3923cf857f8b1ff5f86e0547d54f.

--- Kindlekuniya/Kindlekuniya/settings.py
+++ Kindlekuniya/Kindlekuniya/settings.py
@@ -75,10 +75,8 @@ WSGI_APPLICATION = 'Kindlekuniya.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
-        'HOST': '',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
