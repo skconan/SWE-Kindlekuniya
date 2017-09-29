a2e56df83eed61eaa68421ef7e488880e411836c
skconan <supakit.kr@gmail.com>
2017-09-30 02:34:13 +0700

    created userSiginFormform

--- Kindlekuniya/register/forms.py
+++ Kindlekuniya/register/forms.py
@@ -9,4 +9,6 @@ class userProfileForm(forms.Form):
     confirmPassword = forms.CharField(required=True,max_length=128, widget=forms.PasswordInput)
     phoneNO = forms.IntegerField()
 
-  
+class userSigninForm(forms.Form):
+    email = forms.EmailField(required=True,max_length=128)
+    password = forms.CharField(required=True,max_length=128, widget=forms.PasswordInput)    
