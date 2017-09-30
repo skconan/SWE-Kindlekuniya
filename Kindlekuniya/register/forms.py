68154267a727e4bd7989226ae345a73061c309ce
skconan <supakit.kr@gmail.com>
2017-09-30 10:40:23 +0700

    add clean_email() which is check email is unique

--- Kindlekuniya/register/forms.py
+++ Kindlekuniya/register/forms.py
@@ -1,27 +1,37 @@
 from django import forms
 from django.forms import ModelForm
 from django.core.validators import RegexValidator
-
+from .models import signupModelForm,User
 
 class signupForm(forms.Form):
     email = forms.EmailField(required=True, max_length=128)
-    firstName = forms.CharField(required=True, max_length=128)
-    lastName = forms.CharField(required=True, max_length=128)
+    firstname = forms.CharField(required=True, max_length=128)
+    lastname = forms.CharField(required=True, max_length=128)
     password = forms.CharField(
+        required=True, min_length=8, max_length=128, widget=forms.PasswordInput)
+    confirm_password = forms.CharField(
         required=True, max_length=128, widget=forms.PasswordInput)
-    confirmPassword = forms.CharField(
-        required=True, max_length=128, widget=forms.PasswordInput)
-    phoneNO = forms.CharField(required=True, max_length=10, min_length=10, validators=[RegexValidator(
+    phone_number = forms.CharField(required=True, max_length=10, min_length=10, validators=[RegexValidator(
         regex='^[0-9]*$', message='Phone number must be numeric 0-9 and length is 10'), ])
 
-    def clean(self):
+    def clean_confirm_password(self):
         password1 = self.cleaned_data.get('password')
-        password2 = self.cleaned_data.get('confirmPassword')
+        password2 = self.cleaned_data.get('confirm_password')
 
         if password1 and password1 != password2:
             raise forms.ValidationError("Passwords don't match")
 
         return self.cleaned_data
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email):
+            raise forms.ValidationError("Email don't unique")
+        return self.cleaned_data
+        
+    
+    # def clean_phoneNO(self):   
+    #     phoneNO = self.cleaned_data.get('phoneNO')
 
 
 class signinForm(forms.Form):
